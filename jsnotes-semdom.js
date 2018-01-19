f72aac407bb9365c2e125fdbbad886485c509505
deise.koreeda@gmail.com <deise.koreeda@gmail.com>
2018-01-19 19:15:47 -0200

    inicio create sem dom

--- jsnotes-semdom.js
+++ jsnotes-semdom.js
@@ -2,19 +2,45 @@ console.log("initialize");
 
 document.querySelector("#register").addEventListener("click", () => {
     console.log("clicked");
+    
     const obj = {};
-    obj.title = document.getElementById('note-input-title');
+    obj.title = document.getElementById('note-input-title').value;
     obj.items = document.querySelectorAll('.items-list__input');
+    console.log(obj);
 
-    validateItems(obj);
+    const appendObj = validateItems(obj);
+
+    if(appendObj) {
+        createPostIt(obj);
+    } else {
+
+    }
 });
 
 const validateItems = (obj) => {
-    if(!obj.title) {
-        return false;
-    } else if(Object.values(items).length === 0) {
+
+    let empty = 0;
+
+    Object.values(obj.items).forEach(item => {
+        if(item.value.trim().length === 0) {
+            empty += 1;
+        }
+    });
+
+    if(empty !== 0 || !obj.title) {
         return false;
     } else {
         return true;
     }
 };
+
+const createPostIt = (obj) => {
+    let template = `<h1>${obj.title}</h1>`;
+
+    Object.values(obj.items).forEach(item => {
+        template += `<p>${item.value}</p>`;
+    });
+
+    console.log(template);
+    document.body.appendChild(template);
+}
